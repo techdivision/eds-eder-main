ca2092dadb39f953fab47c2677982a2801f5f995
Simon Sippert <s.sippert@techdivision.com>
2024-06-26 09:47:05 +0200

    EDERCMS-148 Implement usercentrics

---
+++ scripts/load-thirdparty-script.js
@@ -0,0 +1,52 @@
+import { betterLoadScript } from './load-resource.js';
+import { getCurrentUrl } from './helpers.js';
+
+/**
+ * Define partytown config
+ */
+function definePartytownConfig() {
+  window.partytown = {
+    debug: getCurrentUrl()
+      .includes('localhost'),
+    lib: '/scripts/vendor/partytown/',
+  };
+}
+
+/**
+ * Load partytown
+ *
+ * @returns {Promise}
+ */
+function loadPartytown() {
+  definePartytownConfig();
+  return betterLoadScript(
+    '../../../scripts/vendor/partytown/partytown.js',
+    { type: 'module' },
+  );
+}
+
+/**
+ * Load third party script
+ *
+ * @param {String} script
+ * @param {Object} attrs
+ * @returns {Promise}
+ */
+function loadThirdPartyScript(script, attrs) {
+  return loadPartytown()
+    .then(() => {
+      // add partytown attributes to attrs
+      const attributes = {
+        ...attrs,
+        type: 'text/partytown',
+        async: '',
+      };
+
+      // load via partytown
+      betterLoadScript(script, attributes)
+        .then();
+      window.dispatchEvent(new CustomEvent('ptupdate'));
+    });
+}
+
+export default loadThirdPartyScript;
