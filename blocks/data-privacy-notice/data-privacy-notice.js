5c1ae800b1d935001f6025e88f63c64233ecb3f2
Simon Sippert <s.sippert@techdivision.com>
2024-07-25 15:22:43 +0200

    Fix lighthouse score

--- blocks/data-privacy-notice/data-privacy-notice.js
+++ blocks/data-privacy-notice/data-privacy-notice.js
@@ -1,6 +1,20 @@
-import { betterLoadScript } from '../../scripts/load-resource.js';
+import { getCurrentLanguage } from '../../scripts/i18n.js';
+import loadThirdpartyScript from '../../scripts/load-thirdparty-script';
 
-const dataAlfidclUrl = 'https://app.alfright.eu/hosted/dps/alfidcl.js';
+/**
+ * Get language
+ *
+ * @returns {string}
+ */
+function getLanguage() {
+  const language = getCurrentLanguage();
+  if (language === 'fr') {
+    return 'fr-fr';
+  } else if (language === 'en') {
+    return 'en-en';
+  }
+  return 'de-de';
+}
 
 /**
  * Decorate data privacy notice block
@@ -8,13 +22,16 @@ const dataAlfidclUrl = 'https://app.alfright.eu/hosted/dps/alfidcl.js';
  * @param {HTMLElement} block
  */
 export default async function decorate(block) {
-  const dataAlfidclKey = block.querySelector('p').textContent;
-  betterLoadScript(dataAlfidclUrl, { defer: '', 'alfidcl-script': '' }).then(() => {
-    const event = new Event('DOMContentLoaded');
-    block.innerHTML = '<div data-alfidcl-type="dps" '
-      + 'data-alfidcl-tenant="intrasys_scan" data-alfidcl-lang="de-de" '
-      + `data-alfidcl-key="${dataAlfidclKey}"></div>`;
+  const privacyKey = block.querySelector('p').textContent;
 
-    document.dispatchEvent(event);
-  });
+  const privacyDiv = document.createElement('div');
+  privacyDiv.setAttribute('data-alfidcl-type', 'dps');
+  privacyDiv.setAttribute('data-alfidcl-tenant', 'intrasys_scan');
+  privacyDiv.setAttribute('data-alfidcl-lang', getLanguage());
+  privacyDiv.setAttribute('data-alfidcl-key', privacyKey);
+  block.innerHTML = '';
+  block.appendChild(privacyDiv);
+
+  loadThirdpartyScript('https://app.alfright.eu/hosted/dps/alfidcl.js')
+    .then();
 }
