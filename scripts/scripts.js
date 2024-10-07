39981573b8ee7aa6f67420147a362e2f789249cd
Simon Sippert <s.sippert@techdivision.com>
2024-10-07 16:01:17 +0200

    Remove obsolete question mark

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -358,7 +358,7 @@ function updateMetaTitle(doc) {
 async function loadEager(doc) {
   // ensure correct domain
   if (['hlx.page', 'hlx.live'].includes(getTLD())) {
-    (window.top?.location || window?.location).replace(
+    (window.top?.location || window.location).replace(
       getCurrentUrl()
         .replace('hlx.page', 'aem.page')
         .replace('hlx.live', 'aem.live'),
