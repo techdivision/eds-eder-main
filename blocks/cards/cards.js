e2a1d4b515c374e09ccf6176823aa75fe06fdb6a
Dominik Morbitzer <d.morbitzer@techdivision.com>
2024-10-14 17:08:23 +0200

    EDERCMS-233: Fix Linter

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -30,7 +30,6 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
-    const link = row.querySelector('a');
     const li = document.createElement('li');
     copyAttributes(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
